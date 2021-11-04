33352b699d6c46c75fb6356e1c2191890d2100ae
treanter1 <treanter1@gmail.com>
2021-11-04 08:11:49 -0700

    Version 1.1 (DropDowns are Dynamic)

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 const transferBtn = document.querySelector(".transferBtn")
 
-// User submitted values
+// User submitted values Element
 const amount = document.querySelector("#amount");
-const fromValue = document.querySelector("#from").value;
-const toValue = document.querySelector("#to").value;
+const fromEl = document.querySelector("#from");
+const toEl = document.querySelector("#to");
+
+// This Fetch is to dynamically get the Currency List for DropDown
+function firstFetch(){
+    const API = "72601f3aa440257571a608be"
+    const url = `https://v6.exchangerate-api.com/v6/${API}/latest/INR`
+    fetch(url)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        const currencies = data.conversion_rates;
+        addUI(currencies)
+    })
+}
+
+// Running in the start to fetch currencies
+firstFetch()
+
+// Adding Options to the Currency List
+function addUI(currencies) {
+
+    Object.keys(currencies).forEach((el) => {
+        fromEl.add(new Option(el, el), undefined)
+        toEl.add(new Option(el, el), undefined)
+    })
+}
 
 transferBtn.addEventListener("click", fetchResults)
 
-function currencyCalc(from, to){
-    console.log(amount.value * from.conversion_rates[toValue])
-    console.log(from, to)
-}
-
+// Get the selected currencies and their values
 function fetchResults() {
     const API = "72601f3aa440257571a608be"
     const url = `https://v6.exchangerate-api.com/v6/${API}/latest/`
-    Promise.all([ fetch(`${url}${fromValue}`), fetch(`${url}${toValue}`)])
+    Promise.all([ fetch(`${url}${fromEl.value}`), fetch(`${url}${toEl.value}`)])
 
     .then((responses) => {
         return Promise.all(responses.map(function (response) {
@@ -24,8 +46,13 @@ function fetchResults() {
     })
 
     .then((data) => {
-        const fromValue = data[0];
-        const toValue = data[1];
-        currencyCalc(fromValue, toValue)
+        const fromEl = data[0];
+        const toEl = data[1];
+        currencyCalc(fromEl, toEl)
+        
     })
+}
+
+function currencyCalc(from, to){
+    console.log(amount.value * from.conversion_rates[toEl.value])
 }
